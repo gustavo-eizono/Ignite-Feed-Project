7f8b1972ff30ebb4e3a28e78b548c7ca1e394005
Gustavo Eizono Cruz <gustavo.eizono@vtex.com>
2025-02-17 11:24:00 -0300

    Aplicando funcionalidade do botão de Aplaudir

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,16 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }){
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleLikeComment(){
+        setLikeCount(likeCount + 1);
+    }
+
     function handleDeleteComment (){
         console.log("deletar");
         onDeleteComment(content);
@@ -25,9 +33,9 @@ export function Comment({ content, onDeleteComment }){
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
